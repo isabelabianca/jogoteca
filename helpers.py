5765a233fb00569849c0e94d4665d1f1eeafcfe8
Isabela Bianca <79585893+isabelabianca@users.noreply.github.com>
2023-03-29 18:04:35 -0300

    first commit

---
+++ helpers.py
@@ -0,0 +1,43 @@
+import os
+from wtforms import validators
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField, SubmitField, PasswordField
+
+from jogoteca import app
+
+
+class FormularioJogo(FlaskForm):
+    nome = StringField(
+        'Nome do Jogo',
+        [validators.DataRequired(), validators.Length(min=1, max=50)])
+    categoria = StringField(
+        'Categoria',
+        [validators.DataRequired(), validators.Length(min=1, max=40)])
+    console = StringField(
+        'Console',
+        [validators.DataRequired(), validators.Length(min=1, max=20)])
+    salvar = SubmitField('Salvar')
+
+
+class FormularioUsuario(FlaskForm):
+    nickname = StringField(
+        'Nickname',
+        [validators.DataRequired(), validators.Length(min=1, max=8)])
+    senha = PasswordField(
+        'Senha',
+        [validators.DataRequired(), validators.Length(min=1, max=100)])
+    login = SubmitField('Login')
+
+
+def recupera_imagem(id):
+    for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
+        if f'capa{id}' in nome_arquivo:
+            return nome_arquivo
+
+    return 'capa_padrao.jpg'
+
+
+def deleta_arquivo(id):
+    arquivo = recupera_imagem(id)
+    if arquivo != 'capa_padrao.jpg':
+        os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
